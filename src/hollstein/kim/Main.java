e039257e382af0be363b6fa7ce6839618aba99d2
WiggelMc <kim.hh@outlook.de>
2022-01-11 00:55:57 +0100

    Fixed Indents

--- src/hollstein/kim/Main.java
+++ src/hollstein/kim/Main.java
@@ -14,7 +14,7 @@ public class Main extends Base {
 
         {
             Polynom[] polys = {/*
-                new Polynom(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}),*/
+                    new Polynom(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}),*/
                     new Polynom(new int[]{1, 0, 1, 1, 1}),
                     new Polynom(new int[]{1, 1, 1, 0, 1}),
             };
--- src/hollstein/kim/run/test/Test1.java
+++ src/hollstein/kim/run/test/Test1.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class Test1 extends Base {
     public static void main(String[] args) {
         Polynom[] polys = {/*
-            new Polynom(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}),*/
+                new Polynom(new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}),*/
                 new Polynom(new int[]{1, 0, 1, 1, 1}),
                 new Polynom(new int[]{1, 1, 1, 0, 1}),
         };
